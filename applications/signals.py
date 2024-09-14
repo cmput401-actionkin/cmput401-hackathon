03ed72b56156a1854c1f5e1dbadc5859c93b3c48
FNov10 <fahadnaveed2002@gmail.com>
2024-09-14 14:38:34 -0600

    Notifications

--- applications/signals.py
+++ applications/signals.py
@@ -1,20 +1,27 @@
 # signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from notifications.models import Notifications
 from applications.models import Applications
 
-@receiver(post_save, sender=Applications)
+@receiver(pre_save, sender=Applications)
 def create_notification_if_status_changed(sender, instance, **kwargs):
-    print(55)
     if kwargs.get('created', False):
         # Skip creation if this is a new instance
         return
 
+ 
     # Check if the status has changed
     old_instance = Applications.objects.get(pk=instance.pk)
+    print(old_instance.status)
+    print(instance.status)
     if old_instance.status != instance.status:
         # Create a notification
         Notifications.objects.create(
+            companyname = old_instance.companyname,
+            position = old_instance.position,
+            date_applied = old_instance.date_applied,
+            status = instance.status,
             message=f'{instance.companyname} Application Status changed to {instance.status}'
         )
+
