a999fe106567d96a25e83a583ed2b9528f941fba
jteengfo <jamesang@ualberta.ca>
2024-09-14 13:00:19 -0600

    updated App.jsx to include useEffect and imported jobServices

--- jobtective/src/App.jsx
+++ jobtective/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import NavBar from '../components/NavBar'
 import JobList from '../components/JobList'
@@ -6,12 +6,23 @@ import jobService from '../services/jobs'
 
 function App() {
 
-  const [jobLists, setJobLists] = useState([])
+  const [jobs, setJobs] = useState([])
+
+  // used for handling asynchronous activity; were calling the GET request in the script 
+  useEffect(() => {
+    jobService
+      .getAll()
+      .then(initialJobs => {
+        setJobs(initialJobs)
+      })
+
+  // empty array indicates that this useeffect is only run once after initial render; if no empty array, run every render
+  }, [])
 
   return (
     <>
       <NavBar/>
-      <JobList/>
+      <JobList jobs = {jobs}/>
     </>
   )
 }
