92f58abf22124a28c72e63d8e094fc3391a92f70
FNov10 <fahadnaveed2002@gmail.com>
2024-09-14 11:58:34 -0600

    Auth setup

--- applications/views.py
+++ applications/views.py
@@ -3,8 +3,10 @@ from rest_framework import viewsets
 from .models import Applications
 from .serializers import ApplicationSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-
+#@permission_classes([AllowAny])
 class ApplicationsViewSet(viewsets.ModelViewSet):
     queryset = Applications.objects.all()
     serializer_class = ApplicationSerializer
